7fdd61a4cf43324c205080cd8459a48c8b2e83ac
Nico Jensch <njcrypted@protonmail.com>
2020-11-10 09:06:04 +0100

    Move theming to /usr/share

--- usr/share/plasma/look-and-feel/org.garuda.classic/contents/layouts/org.kde.plasma.desktop-layout.js
+++
@@ -1,6 +0,0 @@
-loadTemplate("org.garuda.kde.classic")
-
-var desktopsArray = desktopsForActivity(currentActivity());
-for( var j = 0; j < desktopsArray.length; j++) {
-    desktopsArray[j].wallpaperPlugin = 'org.kde.slideshow';
-}
--- usr/share/plasma/look-and-feel/org.garuda.familiar/contents/layouts/org.kde.plasma.desktop-layout.js
+++
@@ -1,6 +0,0 @@
-loadTemplate("org.garuda.kde.familiar")
-
-var desktopsArray = desktopsForActivity(currentActivity());
-for( var j = 0; j < desktopsArray.length; j++) {
-    desktopsArray[j].wallpaperPlugin = 'org.kde.slideshow';
-}
