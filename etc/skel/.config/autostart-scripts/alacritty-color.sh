80ae0e06e1d5ed29d838b82c111cd2a205dcdd40
Nico Jensch <njcrypted@protonmail.com>
2020-11-14 21:11:46 +0100

    Add icon theme applied notice

--- etc/skel/.config/autostart-scripts/alacritty-color.sh
+++ etc/skel/.config/autostart-scripts/alacritty-color.sh
@@ -3,5 +3,6 @@ rm -rf ~/.config/alacritty
 mv ~/.config/alacritty_themed ~/.config/alacritty
 
 /usr/lib/plasma-changeicons BeautyLine
+notify-send "Theming applied! 😊" --icon plasmashell -a "Garuda dr460nized setup"
 
 rm -f ~/.config/autostart-scripts/alacritty-color.sh
