b9c11f356775bad7f972661a6e90273b1a2cc66c
Nico Jensch <njcrypted@protonmail.com>
2020-11-15 11:45:58 +0100

    Set wallpaper at startup

--- etc/skel/.config/autostart-scripts/alacritty-color.sh
+++ etc/skel/.config/autostart-scripts/alacritty-color.sh
@@ -2,6 +2,17 @@
 rm -rf ~/.config/alacritty
 mv ~/.config/alacritty_themed ~/.config/alacritty
 
+dbus-send --session --dest=org.kde.plasmashell --type=method_call /PlasmaShell org.kde.PlasmaShell.evaluateScript 'string:
+var Desktops = desktops();                                                                                                                       
+for (i=0;i<Desktops.length;i++) {
+        d = Desktops[i];
+        d.wallpaperPlugin = "org.kde.image";
+        d.currentConfigGroup = Array("Wallpaper",
+                                    "org.kde.image",
+                                    "General");
+        d.writeConfig("Image", "file:///usr/share/wallpapers/garuda-wallpapers/Ghosts.jpg");
+}'
+
 /usr/lib/plasma-changeicons BeautyLine
 notify-send "Theming applied! 😊" --icon plasmashell -a "Garuda dr460nized setup"
 
