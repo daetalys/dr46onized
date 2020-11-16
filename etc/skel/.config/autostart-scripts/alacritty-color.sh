3440fb96196eb249bf6f298c1d5cde5110d2c7e2
Nico Jensch <njcrypted@protonmail.com>
2020-11-16 17:20:48 +0100

    Remove redundant

--- etc/skel/.config/autostart-scripts/alacritty-color.sh
+++ etc/skel/.config/autostart-scripts/alacritty-color.sh
@@ -1,19 +1,4 @@
 #!/bin/sh
 cp ~/.config/alacritty_themed/alacritty.yml ~/.config/alacritty/alacritty.yml && rm -rf ~/.config/alacritty_themed
 
-dbus-send --session --dest=org.kde.plasmashell --type=method_call /PlasmaShell org.kde.PlasmaShell.evaluateScript 'string:
-var Desktops = desktops();                                                                                                                       
-for (i=0;i<Desktops.length;i++) {
-        d = Desktops[i];
-        d.wallpaperPlugin = "org.kde.image";
-        d.currentConfigGroup = Array("Wallpaper",
-                                    "org.kde.image",
-                                    "General");
-        d.writeConfig("Image", "file:///usr/share/wallpapers/garuda-wallpapers/Ghosts.jpg");
-}'
-
-/usr/lib/plasma-changeicons BeautyLine && notify-send "Theming applied! 😊" --icon plasmashell -a "Garuda dr460nized setup"
-
-killall latte-dock && latte-dock
-
 rm -f ~/.config/autostart-scripts/
