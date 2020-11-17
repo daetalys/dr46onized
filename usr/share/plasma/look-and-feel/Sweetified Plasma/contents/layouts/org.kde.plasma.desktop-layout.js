3659cf78a87a1af6004d8bf40e2efde75f275ee5
Nico Jensch <njcrypted@protonmail.com>
2020-11-17 13:34:28 +0100

    Use layout.js from KDE edition

--- usr/share/plasma/look-and-feel/Sweetified Plasma/contents/layouts/org.kde.plasma.desktop-layout.js
+++ usr/share/plasma/look-and-feel/Sweetified Plasma/contents/layouts/org.kde.plasma.desktop-layout.js
@@ -7,69 +7,36 @@ var layout = {
             ],
             "config": {
                 "/": {
-                    "ItemGeometriesHorizontal": "",
                     "formfactor": "0",
                     "immutability": "1",
                     "lastScreen": "0",
                     "wallpaperplugin": "com.github.zren.inactiveblur"
                 },
                 "/ConfigDialog": {
-                    "DialogHeight": "836",
-                    "DialogWidth": "1600"
+                    "DialogHeight": "540",
+                    "DialogWidth": "720"
+                },
+                "/Configuration": {
+                    "PreloadWeight": "42"
                 },
                 "/General": {
-                    "ToolBoxButtonState": "topcenter",
-                    "ToolBoxButtonX": "429",
-                    "ToolBoxButtonY": "24"
+                    "positions": "2,12,desktop:/Home.desktop,1,1,desktop:/trash.desktop,1,2",
+                    "showToolbox": "false",
+                    "sortMode": "-1"
                 },
                 "/Wallpaper/com.github.zren.inactiveblur/General": {
                     "FillMode": "2",
                     "Image": "file:///usr/share/wallpapers/garuda-wallpapers/Ghosts.jpg",
                     "SlidePaths": "/usr/share/wallpapers"
-                },
+                },    
                 "/Wallpaper/org.kde.image/General": {
-                    "Image": "file:///usr/share/wallpapers/garuda-wallpapers/Ghosts.jpg",
-                    "SlidePaths": "/usr/share/wallpapers"
+                    "Image": "file:///usr/share/wallpapers/garuda-wallpapers/background.jpg"
                 }
             },
             "wallpaperPlugin": "com.github.zren.inactiveblur"
         }
     ],
     "panels": [
-        {
-            "alignment": "left",
-            "applets": [
-                {
-                    "config": {
-                        "/": {
-                            "immutability": "1"
-                        },
-                        "/Configuration/ConfigDialog": {
-                            "DialogHeight": "540",
-                            "DialogWidth": "720"
-                        },
-                        "/Configuration/General": {
-                            "containmentType": "Plasma"
-                        }
-                    },
-                    "plugin": "org.kde.windowappmenu"
-                }
-            ],
-            "config": {
-                "/": {
-                    "formfactor": "2",
-                    "immutability": "1",
-                    "lastScreen": "0",
-                    "wallpaperplugin": "org.kde.image"
-                }
-            },
-            "height": 2,
-            "hiding": "normal",
-            "location": "bottom",
-            "maximumLength": 88.88888888888889,
-            "minimumLength": 88.88888888888889,
-            "offset": 0
-        }
     ],
     "serializationFormatVersion": "1"
 }
